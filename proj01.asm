957a7ac4c6f01349ed692620307a1da705259ee9
Lkaetsu <111883767+Lkaetsu@users.noreply.github.com>
2022-10-28 09:22:43 -0300

    Update proj01.asm

--- proj01.asm
+++ proj01.asm
@@ -1,10 +1,53 @@
-            ;Nome: Leonardo Seiji Kaetsu        RA: 22008336
+TITLE Nome: Leonardo Seiji Kaetsu       RA: 22008336
 .model small
 .data
     msg1 db 'Entre o primeiro numero: $'
     msg2 db 'Entre a operacao desejada(+,-,*,/): $'
     msg3 db 'Entre o segundo numero: $'
 .CODE
+MAIOR_10 PROC
+    AND AX,0
+    MOV AL,BL
+    MOV BL,10
+    DIV BL
+    MOV DL,AH
+    MOV AH,02
+    INT 21h
+    MOV DL,AL
+    INT 21h
+    RET
+MAIOR_10 ENDP
+
+OP_SOMA PROC
+    MOV DL,BL           ;impressao da operacao completa
+    INT 21h
+    MOV DL,'+'
+    INT 21h
+    MOV DL,BH
+    INT 21h
+    MOV DL,'='
+    INT 21h
+
+    SUB BL,30h          ;operacao
+    SUB BH,30h
+    ADD BL,BH
+
+    CMP BL,0Ah
+    JBE M_10             ;se o resultado for maior que dez entra na funcao
+    CALL MAIOR_10
+    JMP FIM1
+    M_10:
+    ADD BL,30h          ;impressao do resultado
+    MOV DL,BL
+    INT 21h
+    FIM1:
+    RET
+OP_SOMA ENDP
+
+OP_SUBTRACAO PROC
+    RET
+OP_SUBTRACAO ENDP
+
 MAIN PROC
     MOV AX,@data            ;inicializacao o segmento de dados
     MOV DS,AX
@@ -26,7 +69,7 @@ MAIN PROC
 
     MOV AH,01               ;armazenamento do caracter da operacao em cl
     INT 21h
-    MOV CL,AL
+    MOV CL,AL             
 
     MOV AH,02               ;pulando linha
     MOV DL,10
@@ -43,25 +86,11 @@ MAIN PROC
     MOV DL,10
     INT 21h
 
-    CMP CL,'+'              
+    CMP CL,'+'
     JNZ Soma                ;se o caracter da operacao for diferente de + pular para a proxima operacao
-        MOV DL,BL           ;impressao da operacao completa
-        INT 21h
-        MOV DL,'+'
-        INT 21h
-        MOV DL,BH
-        INT 21h
-        MOV DL,'='
-        INT 21h
-
-        SUB BL,30h          ;operacao
-        SUB BH,30h
-        ADD BL,BH
-
-        ADD BL,30h          ;impressao do resultado
-        MOV DL,BL
-        INT 21h
-        Soma:
+    CALL OP_SOMA
+    JMP FIM2
+    Soma:
 
     CMP CL,'-'
     JNZ Subtracao           ;se o caracter da operacao for diferente de - pular para a proxima operacao
@@ -88,6 +117,7 @@ MAIN PROC
         MOV DL,BL
         INT 21h
         Subtracao:
+        FIM2:
 
     MOV AH,4Ch              ;exit
     INT 21h
