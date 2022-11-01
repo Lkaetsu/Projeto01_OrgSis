e6d6579deabb3b45b162d7fcf77659b344fef4ac
Lkaetsu <111883767+Lkaetsu@users.noreply.github.com>
2022-11-01 14:20:57 -0300

    Update proj01.asm

--- proj01.asm
+++ proj01.asm
@@ -4,29 +4,39 @@ TITLE Nome: Leonardo Seiji Kaetsu       RA: 22008336
     msg1 db 'Entre o primeiro numero: $'
     msg2 db 'Entre a operacao desejada(+,-,*,/): $'
     msg3 db 'Entre o segundo numero: $'
+    error db 'Erro: Operacao invalida!$'
 .CODE
-MAIOR_10 PROC
-    AND AX,0
-    MOV AL,BL
-    MOV BL,10
-    DIV BL
-    MOV DL,AH
-    MOV AH,02
-    INT 21h
-    MOV DL,AL
-    INT 21h
-    RET
-MAIOR_10 ENDP
-
-OP_SOMA PROC
+IMPRESSAO_OP PROC
     MOV DL,BL           ;impressao da operacao completa
     INT 21h
-    MOV DL,'+'
+    MOV DL,CL
     INT 21h
     MOV DL,BH
     INT 21h
     MOV DL,'='
     INT 21h
+    RET
+IMPRESSAO_OP ENDP
+
+MAIOR_10 PROC
+    MOV AX,0            ;divisao por 10 para separar o numero
+    MOV AL,BL           ;em dois numeros a unidade e a dezena
+    MOV BL,0Ah
+    DIV BL
+
+    MOV BL,AH           ;impressao do numero separadamente
+    ADD AL,30h
+    ADD BL,30h
+    MOV AH,02
+    MOV DL,AL
+    INT 21h
+    MOV DL,BL
+    INT 21h
+    RET
+MAIOR_10 ENDP
+
+OP_SOMA PROC
+    CALL IMPRESSAO_OP
 
     SUB BL,30h          ;operacao
     SUB BH,30h
@@ -45,8 +55,43 @@ OP_SOMA PROC
 OP_SOMA ENDP
 
 OP_SUBTRACAO PROC
+    CALL IMPRESSAO_OP
+
+    SUB BL,30h          ;operacao
+    SUB BH,30h
+    SUB BL,BH
+
+    JNS Negativo        ;se o resultado for negativo transforma-lo em positivo e imprimir um - em sua frente
+        MOV DL,'-'
+        INT 21h
+        NEG BL
+    Negativo:
+            
+    ADD BL,30h          ;impressao do resultado
+    MOV DL,BL
+    INT 21h
     RET
 OP_SUBTRACAO ENDP
+
+OP_MULTIPLICACAO PROC
+    CALL IMPRESSAO_OP
+
+    CMP BL,0Ah
+    JBE M_10             ;se o resultado for maior que dez entra na funcao
+    CALL MAIOR_10
+    JMP FIM1
+    M_10:
+
+    ADD BL,30h          ;impressao do resultado
+    MOV DL,BL
+    INT 21h
+    RET
+OP_MULTIPLICACAO ENDP
+
+OP_DIVISAO PROC
+    CALL IMPRESSAO_OP
+    RET
+OP_DIVISAO ENDP
 
 MAIN PROC
     MOV AX,@data            ;inicializacao o segmento de dados
@@ -89,35 +134,35 @@ MAIN PROC
     CMP CL,'+'
     JNZ Soma                ;se o caracter da operacao for diferente de + pular para a proxima operacao
     CALL OP_SOMA
-    JMP FIM2
+    JMP FIM_SOMA            ;pula para o final do programa
     Soma:
 
     CMP CL,'-'
     JNZ Subtracao           ;se o caracter da operacao for diferente de - pular para a proxima operacao
-        MOV DL,BL           ;impressao da operacao completa
-        INT 21h
-        MOV DL,'-'
-        INT 21h
-        MOV DL,BH
-        INT 21h
-        MOV DL,'='
-        INT 21h
+    CALL OP_SUBTRACAO
+    JMP FIM_SUB             ;pula para o final do programa
+    Subtracao:
 
-        SUB BL,30h          ;operacao
-        SUB BH,30h
-        SUB BL,BH
+    CMP CL,'*'
+    JNZ Multiplicacao       ;se o caracter da operacao for diferente de * pular para a proxima operacao
+    CALL OP_MULTIPLICACAO
+    JMP FIM_MULT            ;pula para o final do programa
+    Multiplicacao:
 
-        JNS Negativo        ;se o resultado for negativo transforma-lo em positivo e imprimir um - em sua frente
-            MOV DL,'-'
-            INT 21h
-            NEG BL
-            Negativo:
-            
-        ADD BL,30h          ;impressao do resultado
-        MOV DL,BL
-        INT 21h
-        Subtracao:
-        FIM2:
+    CMP CL,'/'
+    JNZ Divisao             ;se o caracter da operacao for diferente de / pular para a proxima operacao
+    CALL OP_DIVISAO
+    JMP FIM_DIVI            ;pula para o final do programa
+    Divisao:
+
+    MOV AH,09               ;impressao da mensagem de erro se a operacao nao for uma das aplicaveis
+    LEA DX,error
+    INT 21h
+
+    FIM_SOMA:
+    FIM_SUB:
+    FIM_MULT:
+    FIM_DIVI:
 
     MOV AH,4Ch              ;exit
     INT 21h
